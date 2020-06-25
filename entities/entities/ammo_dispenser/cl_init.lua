f30b546d7606fd4a4ddb238a6f22351629be786b
JeakeG <snakeyes2799@yahoo.com>
2020-06-25 07:39:34 -0500

    made ammo dispenser spawnable

--- entities/entities/ammo_dispenser/cl_init.lua
+++ entities/entities/ammo_dispenser/cl_init.lua
@@ -1,5 +1,5 @@
 include("shared.lua")
 
 function ENT:Draw()
-
+    self:DrawModel()
 end
--- entities/entities/ammo_dispenser/init.lua
+++ entities/entities/ammo_dispenser/init.lua
@@ -4,11 +4,29 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
+    self:SetModel("models/props_lab/reciever_cart.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
 
+    local phys = self:GetPhysicsObject()
+
+    if (IsValid(phys)) then
+        phys:Wake()
+    end
 end
 
 function ENT:SpawnFunction(player, tr, ClassName)
+    if (!tr.Hit) then return end
 
+    local spawnPos = player:GetShootPos() + player:GetForward() * 80
+
+    local ent = ents.Create(ClassName)
+    ent:SetPos(spawnPos)
+    ent:Spawn()
+    ent:Activate()
+
+    return ent
 end
 
 function ENT:Use(activator, caller)
