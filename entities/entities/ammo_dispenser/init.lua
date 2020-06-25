2c77554a98b119d654ac61bba4f63170d8c4c135
JeakeG <snakeyes2799@yahoo.com>
2020-06-25 07:32:55 -0500

    added base files for entity ammo dispenser

---
+++ entities/entities/ammo_dispenser/init.lua
@@ -0,0 +1,20 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+
+end
+
+function ENT:SpawnFunction(player, tr, ClassName)
+
+end
+
+function ENT:Use(activator, caller)
+    --Whenever a player uses the entity
+end
+
+function ENT:Think()
+    --Called every tick
+end
---
+++ entities/entities/ammo_dispenser/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Buildings"
+
+ENT.PrintName = "Ammo Dispenser"
+ENT.Author = "Jeake"
+ENT.Purpose = "Give Ammo"
+ENT.Instructions = "Place and Use"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false 
